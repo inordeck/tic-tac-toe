743682b3795d4cee2a6e586c8b3856a65452a5fb
inordeck <ian.nordeck@gmail.com>
2017-07-11 01:31:00 -0600

    fix game logic based on tutorial

--- js/app.js
+++ js/app.js
@@ -1,111 +1,78 @@
-/* CREATE PLAYER */
+/* START GAME */
+function startGame() {
+	for (var i = 1; i <= 9; i = i + 1) { /* LOOP TO CLEAR CELLS */
+		clearCell(i);
+	}
+	document.turn = "X"; /* SET TURN */
+	if (Math.random() < 0.5) { /* USE MATH.RANDOM TO SWITCH START */
+		document.turn = "O";
+	}
+	document.winner = null; /* MAKE SURE NO ONE HAS WON */
+	setMessage(document.turn + " starts the game."); /* WHO STARTS GAME */
+}
 
-	var playerHeadline = document.getElementsByClassName("playerTurn");
-	var playerOne = document.createElement('h3');
-	var oneText = document.createTextNode("Player one's turn");
+/* SET MESSAGE */
+function setMessage(msg) {
+	document.getElementById("message").innerText = msg;
+}
 
-var playerOneTurn = function() {
-	playerOne.setAttribute('id', "playerOneTurn");
-	playerOne.appendChild(oneText);
-	playerOne.style.color = "#f7941d";
-};
-playerOneTurn();
+/* NEXT MOVE */
+function nextMove(cell) {
+	if (document.winner != null) { /* IF THERE IS A WINNER */
+		setMessage(document.winner + " already won."); /* DILPLSY WINNER */
+	} else if (cell.innerText == "") { /* IF NO WNNER, START NEXT PLAYER'S TURN */
+		cell.innerText = document.turn;
+		switchTurn();
+	} else { /* IF BOX IS SELECTE, TELL PLAYER TO SELECT ANOTHER BOX */
+		setMessage("Select another square.");
+	}
+}
 
+/* SWITCH TURNS */
+function switchTurn() {
+	if (checkForWinner(document.turn)) { /* CHECK FOR WINNER */
+		setMessage("Conratulations, " + document.turn + " wins!"); /* DISPLAY WINNER IN MESSAGE */
+		document.winner = document.turn;
+	} else if (document.turn == "X") { /* IF NO WINNER. SWITCH TURNS */
+		document.turn = "O";
+		setMessage("It's " + document.turn + "'s turn."); /* DISPLAY MESSAGE OF PLAYER'S TURN */
+	} else {
+		document.turn = "X"; /* SWITCH TO OTHER PLAYER */
+		setMessage("It's " + document.turn + "'s turn."); /* DISPLAY MESSAGE OF PLAYER'S TURN */
+	}
+}
 
-/* PLAYER 1: X PIECE */
-/* (add to function to create X)
-	var x = document.createElement('h2');
-	var xText = document.createTextNode("X");
-	x.setAttribute('id', "x");
-	x.appendChild(xText);
-	space1.style.backgroundColor = "#f7941d";
-	console.log(x);
-	space1.appendChild(x);
-*/
+/* CHECK FOR WINNING COMBINATIONS */
+function checkForWinner(move) {
+	var result = false;
+	if (checkRow(1, 2, 3, move) ||
+		checkRow(4, 5, 6, move) ||
+		checkRow(7, 8, 9, move) ||
+		checkRow(1, 4, 7, move) ||
+		checkRow(2, 5, 8, move) ||
+		checkRow(3, 6, 9, move) ||
+		checkRow(1, 5, 9, move) ||
+		checkRow(3, 5, 7, move)) {
+		result = true;
+	}
+	return result;
+}
 
-/* PLAYER 2: O PIECE */
-/* (add to function to create O)
-	var o = document.createElement('h2');
-	var oText = document.createTextNode("O");
-	o.setAttribute('id', "o");
-	o.appendChild(oText);
-	space2.style.backgroundColor = "#12a89d";
-	console.log(o);
-	space2.appendChild(o);
-	space2.removeEventListener('click', space2, false);
-	console.log("clicked");
-*/
+/* FUNCTION TO CHECK 3 VALUES AND MOVE TO CHECH FOR WINNER */
+function checkRow(a, b, c, move) {
+	var result = false;
+	if (getCell(a) == move && getCell(b) == move && getCell(c) == move) {
+		result = true;
+	}
+	return result;
+}
 
+/* GET CELL NUMBER TO USE IN CHECK ROW */
+function getCell(number) {
+	return document.getElementById("cell" + number).innerText;
+}
 
-/* ROW 1 */
-var space1 = document.getElementById("cell1");
-console.log(space1);
-space1.addEventListener("click", function() {
-	var x = document.createElement('h2');
-	var xText = document.createTextNode("X");
-	x.setAttribute('id', "x");
-	x.appendChild(xText);
-	space1.style.backgroundColor = "#f7941d";
-	console.log(x);
-	space1.appendChild(x);
-	console.log("clicked");
-});
-
-var space2 = document.getElementById("cell2");
-console.log(space2);
-space2.addEventListener("click", function() {
-	var o = document.createElement('h2');
-	var oText = document.createTextNode("O");
-	o.setAttribute('id', "o");
-	o.appendChild(oText);
-	space2.style.backgroundColor = "#12a89d";
-	console.log(o);
-	space2.appendChild(o);
-	space2.removeEventListener('click', space2, false);
-	console.log("clicked");
-	console.log("clicked");
-});
-
-var space3 = document.getElementById("cell3");
-console.log(space3);
-space3.addEventListener("click", function() {
-	console.log("clicked");
-});
-
-/* ROW 2 */
-var space4 = document.getElementById("cell4");
-console.log(space4);
-space4.addEventListener("click", function() {
-	console.log("clicked");
-});
-
-var space5 = document.getElementById("cell5");
-console.log(space5);
-space5.addEventListener("click", function() {
-	console.log("clicked");
-});
-
-var space6 = document.getElementById("cell6");
-console.log(space6);
-space6.addEventListener("click", function() {
-	console.log("clicked");
-});
-
-/* ROW 3 */
-var space7 = document.getElementById("cell7");
-console.log(space7);
-space7.addEventListener("click", function() {
-	console.log("clicked");
-});
-
-var space8 = document.getElementById("cell8");
-console.log(space8);
-space8.addEventListener("click", function() {
-	console.log("clicked");
-});
-
-var space9 = document.getElementById("cell9");
-console.log(space9);
-space9.addEventListener("click", function() {
-	console.log("clicked");
-});
+/* CLEAR CELLS AFTER GAME IS FINISHE CND HAS BEEN RESET */
+function clearCell(number) {
+	document.getElementById("cell" + number).innerText = "";
+}
